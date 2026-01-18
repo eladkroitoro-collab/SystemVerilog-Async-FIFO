0cf24054265d9ef2225d223bb941974e2b688472
eladkroitoro-collab <eladkroitoro@gmail.com>
2026-01-18 13:16:05 +0200

    Update testbench.sv

--- Verification/testbench.sv
+++ Verification/testbench.sv
@@ -1,18 +1,14 @@
 `timescale 1ns/1ps
-
 module tb_async_fifo;
-
     // Parameters 
     parameter DATA_WIDTH = 8;
     parameter DEPTH      = 16;
-
     // DUT Signals
     logic w_clk, w_rst_n, w_en;
     logic [DATA_WIDTH-1:0] w_data;
     logic r_clk, r_rst_n, r_en;
     logic [DATA_WIDTH-1:0] r_data;
     logic w_full, r_empty;
-
     async_fifo #(
         .DATA_WIDTH(DATA_WIDTH),
         .DEPTH(DEPTH)
@@ -35,8 +31,8 @@ module tb_async_fifo;
         // Waveform Setup
         $dumpfile("dump.vcd");
         $dumpvars(0, tb_async_fifo);
-
-        // Reset Sequence
+        
+        // 1.Test Case: Reset Sequence
         $display("Starting Reset ");
         w_rst_n = 0; r_rst_n = 0;
         w_en = 0; r_en = 0; w_data = 0;
@@ -44,36 +40,28 @@ module tb_async_fifo;
         w_rst_n = 1; r_rst_n = 1; // Release Reset
         $display("Reset Done");
         #20;
-
-        // 2. Test Case: Filling the FIFO
-        $display("Test 1: Filling the FIFO");
         
+        // 2. Test Case: Filling the FIFO
+        $display("Test 1: Filling the FIFO"); 
         for (int i = 0; i < 16; i++) begin
             wait(!w_full); 
-            
             @(posedge w_clk); 
             w_en = 1;
             w_data = i * 10; // Pattern: 0, 10, 20...
-            
             @(posedge w_clk);
             w_en = 0;
         end
-
         $display("Finished writing 16 items");
         #100; 
-
+        
         // 3. Test Case: Emptying the FIFO
         $display("Test 2: Emptying the FIFO");
-
         for (int i = 0; i < 16; i++) begin
-            wait(!r_empty);
-            
+            wait(!r_empty);   
             @(posedge r_clk);
-            r_en = 1;
-            
+            r_en = 1;  
             @(posedge r_clk);
             r_en = 0;
-            
             $display("Read Data: %d (Time: %t)", r_data, $time);
         end
 
@@ -81,5 +69,4 @@ module tb_async_fifo;
         $display(" Test Done");
         $finish;
     end
-
 endmodule
