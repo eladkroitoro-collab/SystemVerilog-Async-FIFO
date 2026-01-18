ac076725673aca1758610b576aa7cb6b22d44171
eladkroitoro-collab <eladkroitoro@gmail.com>
2026-01-18 13:06:55 +0200

    Update gray2bin.sv

--- RTL/gray2bin.sv
+++ RTL/gray2bin.sv
@@ -1,4 +1,4 @@
-// Module 2: Gray to Binary Converter
+// Gray to Binary Converter
 // Converts Gray code back to binary for pointer
 module gray2bin #(parameter WIDTH = 5)
 (
@@ -7,8 +7,7 @@ module gray2bin #(parameter WIDTH = 5)
 );
     always_comb begin
         // The MSB is always identical in both representations
-        bin_out[WIDTH-1] = gray_in[WIDTH-1];
-        
+        bin_out[WIDTH-1] = gray_in[WIDTH-1];  
         // Iterate from MSB-1 down to LSB using XOR chain
         for (int i = WIDTH-2; i >= 0; i--) begin
             bin_out[i] = gray_in[i] ^ bin_out[i+1];
